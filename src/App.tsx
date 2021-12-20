41aa2c4d2f44103be6523fa240a423fd7a1cdf10
Luiz de Lima <luiz12359@gmail.com>
2021-12-20 19:03:21 -0300

    instalando styled components

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+const Title=styled.h1`
+color:#a699c0;
+button{
+  color:#000;
+  background-color:orangered;
+}
+`
+export function App() {
+  return (
+    <div className="App">
+     <Title>Iae
+       <button>oi</button>
+     </Title>
+      
+    </div>
+  );
+}
+
+
