3967f09ab0c2f0e531ca4a93022682c993b72455
Luiz de Lima <luiz12359@gmail.com>
2021-12-20 23:41:23 -0300

    adicionando as imagens

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,9 @@
 import styled from 'styled-components'
 
-const Title=styled.h1`
-color:#a699c0;
-button{
-  color:#000;
-  background-color:orangered;
-}
-`
 export function App() {
   return (
     <div className="App">
-     <Title>Iae
-       <button>oi</button>
-     </Title>
-      
+           
     </div>
   );
 }
