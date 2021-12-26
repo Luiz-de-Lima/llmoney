1fb09a81edd29973a0cfd6d24aac50ab4eb9bb14
Luiz de Lima <luiz12359@gmail.com>
2021-12-26 12:56:16 -0300

    estilo global e fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import styled from 'styled-components'
+import { GlobalStyle } from './styles/global';
 
+const Title = styled.h1`
+font-size:38px;
+color:#8257e6;`
 export function App() {
   return (
     <div className="App">
-           
+<Title>
+  Oie
+</Title>
+<GlobalStyle/>
+
     </div>
   );
 }
